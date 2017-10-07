2ba2a7245d5e1708723c47546058f700628bc8ed
Leonardo Henrique Batista <leonardohenriquebatista@gmail.com>
2017-10-07 20:51:48 -0300

    Lab 05 entregue, Lab06 iniciado e README atualizado

---
+++ 06/hamming-template.s
@@ -0,0 +1,164 @@
+.globl _start
+
+.data
+
+input_buffer:   .skip 32
+output_buffer:  .skip 32
+    
+.text
+.align 4
+
+@ Funcao inicial
+_start:
+    @ Chama a funcao "read" para ler 4 caracteres da entrada padrao
+    ldr r0, =input_buffer
+    mov r1, #5             @ 4 caracteres + '\n'
+    bl  read
+    mov r4, r0             @ copia o retorno para r4.
+
+    @ Chama a funcao "atoi" para converter a string para um numero
+    ldr r0, =input_buffer
+    mov r1, r4
+    bl  atoi
+
+    @ Chama a funcao "encode" para codificar o valor de r0 usando
+    @ o codigo de hamming.
+    bl  encode
+    mov r4, r0             @ copia o retorno para r4.
+	
+    @ Chama a funcao "itoa" para converter o valor codificado
+    @ para uma sequencia de caracteres '0's e '1's
+    ldr r0, =output_buffer
+    mov r1, #7
+    mov r2, r4
+    bl  itoa
+
+    @ Adiciona o caractere '\n' ao final da sequencia (byte 7)
+    ldr r0, =output_buffer
+    mov r1, #'\n'
+    strb r1, [r0, #7]
+
+    @ Chama a funcao write para escrever os 7 caracteres e
+    @ o '\n' na saida padrao.
+    ldr r0, =output_buffer
+    mov r1, #8         @ 7 caracteres + '\n'
+    bl  write
+
+    @ Chama a funcao exit para finalizar processo.
+    mov r0, #0
+    bl  exit
+
+@ Codifica o valor de entrada usando o codigo de hamming.
+@ parametros:
+@  r0: valor de entrada (4 bits menos significativos)
+@ retorno:
+@  r0: valor codificado (7 bits como especificado no enunciado).
+encode:    
+       push {r4-r11, lr}
+       
+       @ <<<<<< ADICIONE SEU CODIGO AQUI >>>>>>
+    
+       pop  {r4-r11, lr}
+       mov  pc, lr
+
+@ Decodifica o valor de entrada usando o codigo de hamming.
+@ parametros:
+@  r0: valor de entrada (7 bits menos significativos)
+@ retorno:
+@  r0: valor decodificado (4 bits como especificado no enunciado).
+@  r1: 1 se houve erro e 0 se nao houve.
+decode:    
+       push {r4-r11, lr}
+       
+       @ <<<<<< ADICIONE SEU CODIGO AQUI >>>>>>
+
+       pop  {r4-r11, lr}
+       mov  pc, lr
+
+@ Le uma sequencia de bytes da entrada padrao.
+@ parametros:
+@  r0: endereco do buffer de memoria que recebera a sequencia de bytes.
+@  r1: numero maximo de bytes que pode ser lido (tamanho do buffer).
+@ retorno:
+@  r0: numero de bytes lidos.
+read:
+    push {r4,r5, lr}
+    mov r4, r0
+    mov r5, r1
+    mov r0, #0         @ stdin file descriptor = 0
+    mov r1, r4         @ endereco do buffer
+    mov r2, r5         @ tamanho maximo.
+    mov r7, #3         @ read
+    svc 0x0
+    pop {r4, r5, lr}
+    mov pc, lr
+
+@ Escreve uma sequencia de bytes na saida padrao.
+@ parametros:
+@  r0: endereco do buffer de memoria que contem a sequencia de bytes.
+@  r1: numero de bytes a serem escritos
+write:
+    push {r4,r5, lr}
+    mov r4, r0
+    mov r5, r1
+    mov r0, #1         @ stdout file descriptor = 1
+    mov r1, r4         @ endereco do buffer
+    mov r2, r5         @ tamanho do buffer.
+    mov r7, #4         @ write
+    svc 0x0
+    pop {r4, r5, lr}
+    mov pc, lr
+
+@ Finaliza a execucao de um processo.
+@  r0: codigo de finalizacao (Zero para finalizacao correta)
+exit:    
+    mov r7, #1         @ syscall number for exit
+    svc 0x0
+
+@ Converte uma sequencia de caracteres '0' e '1' em um numero binario
+@ parametros:
+@  r0: endereco do buffer de memoria que armazena a sequencia de caracteres.
+@  r1: numero de caracteres a ser considerado na conversao
+@ retorno:
+@  r0: numero binario
+atoi:
+    push {r4, r5, lr}
+    mov r4, r0         @ r4 == endereco do buffer de caracteres
+    mov r5, r1         @ r5 == numero de caracteres a ser considerado 
+    mov r0, #0         @ number = 0
+    mov r1, #0         @ loop indice
+atoi_loop:
+    cmp r1, r5         @ se indice == tamanho maximo
+    beq atoi_end       @ finaliza conversao
+    mov r0, r0, lsl #1 
+    ldrb r2, [r4, r1]  
+    cmp r2, #'0'       @ identifica bit
+    orrne r0, r0, #1   
+    add r1, r1, #1     @ indice++
+    b atoi_loop
+atoi_end:
+    pop {r4, r5, lr}
+    mov pc, lr
+
+@ Converte um numero binario em uma sequencia de caracteres '0' e '1'
+@ parametros:
+@  r0: endereco do buffer de memoria que recebera a sequencia de caracteres.
+@  r1: numero de caracteres a ser considerado na conversao
+@  r2: numero binario
+itoa:
+    push {r4, r5, lr}
+    mov r4, r0
+itoa_loop:
+    sub r1, r1, #1         @ decremento do indice
+    cmp r1, #0          @ verifica se ainda ha bits a serem lidos
+    blt itoa_end
+    and r3, r2, #1
+    cmp r3, #0
+    moveq r3, #'0'      @ identifica o bit
+    movne r3, #'1'
+    mov r2, r2, lsr #1  @ prepara o proximo bit
+    strb r3, [r4, r1]   @ escreve caractere na memoria
+    b itoa_loop
+itoa_end:
+    pop {r4, r5, lr}
+    mov pc, lr    
--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Repositório das atividades realizadas durante a disciplina MC404 - Organizaçã
 * Lab 2 - **Balística** - Calcular a velocidade inicial de um projetil pra atingir determinada distância
 * Lab 3 - **Laços e Vetores** - Calcular o produto escalar de dois vetores
 * Lab 4 - **Escrevendo em linguagem de montagem** - Escrever os Labs 02 e 03 em linguagem de montagem do IAS
+* Lab 5 - **Introdução ao ARM** - Familiarização com o ambiente de montagem, ligação e execução de programas na infraestrutura do sistema ARM.
+* Lab 6 - **Código de Hamming** - Implementar as funções de *Encode* e *Decode*
 
 * Trabalho 1 - **Montador para o IAS** - Parte 1: Processamento da entrada / Parte 2: Geracao do mapa de memoria
